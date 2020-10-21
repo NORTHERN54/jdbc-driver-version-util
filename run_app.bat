f7a6ecb1da5218ea686513ea934273497fbf19a9
Yury_Verameichyk <Yury_Verameichyk@epam.com>
2020-10-22 00:30:02 +0300

    run app

---
+++ run_app.bat
@@ -0,0 +1,2 @@
+cd .\out\artifacts\JdbcDriverVersionUtil_jar\
+C:\jdk1.5.0_22\bin\java.exe -jar .\JdbcDriverVersionUtil.jar jdbc:sqlserver://zuse2dmsblddbs01.4f4bacceafad.database.windows.net:1433;databaseName=MSC_***;user=MSC_User***;password=***;encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.4f4bacceafad.database.windows.net;
